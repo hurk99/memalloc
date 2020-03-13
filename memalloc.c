4be7fa840519a823d6e0c82620553ab6204bb45d
hurk99 <59738944+hurk99@users.noreply.github.com>
2020-03-13 16:17:21 -0400

    Update memalloc.c

--- memalloc.c
+++ memalloc.c
@@ -1,167 +1,154 @@
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
-/* Only for the debug printf */
-#include <stdio.h>
 
-typedef char ALIGN[16];
-
-union header {
-	struct {
-		size_t size;
-		unsigned is_free;
-		union header *next;
-	} s;
-	/* force the header to be aligned to 16 bytes */
-	ALIGN stub;
-};
-typedef union header header_t;
-
-header_t *head = NULL, *tail = NULL;
-pthread_mutex_t global_malloc_lock;
-
-header_t *get_free_block(size_t size)
+typedef struct block
 {
-	header_t *curr = head;
-	while(curr) {
-		/* see if there's a free block that can accomodate requested size */
-		if (curr->s.is_free && curr->s.size >= size)
-			return curr;
-		curr = curr->s.next;
-	}
-	return NULL;
-}
+	size_t size;		// How many bytes beyond this block have been allocated in the heap
+	struct block *next; // Where is the next block in your linked list
+	int free;			// Is this memory free?
+} block_t;
 
-void free(void *block)
+#define BLOCK_SIZE sizeof(block_t)
+
+// Global variables to keep track of
+block_t *head = NULL;
+block_t *tail = NULL;
+pthread_mutex_t global_lock;
+
+// A function that would find the best free block for a given size. It will return NULL when nothing has been found.
+block_t *findFreeBlock(size_t s)
 {
-	header_t *header, *tmp;
-	/* program break is the end of the process's data segment */
-	void *programbreak;
+	block_t *curr = head;
+	block_t *best = NULL;
+	int counter = 0;
 
-	if (!block)
-		return;
-	pthread_mutex_lock(&global_malloc_lock);
-	header = (header_t*)block - 1;
-	/* sbrk(0) gives the current program break address */
-	programbreak = sbrk(0);
-
-	/*
-	   Check if the block to be freed is the last one in the
-	   linked list. If it is, then we could shrink the size of the
-	   heap and release memory to OS. Else, we will keep the block
-	   but mark it as free.
-	 */
-	if ((char*)block + header->s.size == programbreak) {
-		if (head == tail) {
-			head = tail = NULL;
-		} else {
-			tmp = head;
-			while (tmp) {
-				if(tmp->s.next == tail) {
-					tmp->s.next = NULL;
-					tail = tmp;
-				}
-				tmp = tmp->s.next;
+	// Iterate throughout the list
+	while (curr != NULL)
+	{
+		// If the currrent block is free and the size is greater or equal to the size we want to find...
+		if (curr->free && curr->size >= s)
+		{
+			// If there is no recorded best block, make the current block the best block
+			if (best == NULL)
+			{
+				best = curr;
+			}
+			// Else if the best block's size is bigger than the current block, update the best block
+			else if (best->size > curr->size)
+			{
+				best = curr;
 			}
 		}
-		/*
-		   sbrk() with a negative argument decrements the program break.
-		   So memory is released by the program to OS.
-		*/
-		sbrk(0 - header->s.size - sizeof(header_t));
-		/* Note: This lock does not really assure thread
-		   safety, because sbrk() itself is not really
-		   thread safe. Suppose there occurs a foregin sbrk(N)
-		   after we find the program break and before we decrement
-		   it, then we end up realeasing the memory obtained by
-		   the foreign sbrk().
-		*/
-		pthread_mutex_unlock(&global_malloc_lock);
+		// Advance the current blokc
+		curr = curr->next;
+		counter++;
+	}
+	// Return the best block.
+	return best;
+}
+
+// A malloc function that find the best-fit available spaces
+void *mymalloc(size_t s)
+{
+	// If the size that is being requested is 0, it will return null.
+	if (s == 0)
+	{
+		return NULL;
+	}
+
+	// We get the lock before continuing.
+	pthread_mutex_lock(&global_lock);
+
+	// We have the free block be the variable header
+	block_t *freeBlock;
+	freeBlock = findFreeBlock(s);
+
+	// If there is a free block...
+	if (freeBlock != NULL)
+	{
+		// Then the free block is no longer free, and we return the free block.
+		freeBlock->free = 0;
+		pthread_mutex_unlock(&global_lock);
+		printf("malloc %zu bytes\n", s);
+
+		// We return freeBlock + 1, as we don't want to include the freeBlock itself
+		return freeBlock + 1;
+	}
+
+	// If the free block is not found...
+	else
+	{
+		// We increase the heap to the new totalSize.
+		block_t *newBlock;
+		newBlock = sbrk(BLOCK_SIZE + s);
+
+		// If the sbrk does work...
+		if (newBlock != NULL)
+		{
+			// Update the freeBlock to be the other block.
+			freeBlock = newBlock;
+			freeBlock->size = s;
+			freeBlock->free = 0;
+			freeBlock->next = NULL;
+
+			// If the head has not been established, the new block is the head
+			if (head == NULL)
+			{
+				head = freeBlock;
+			}
+
+			// If the tail is already established, link the previous tail to new tail
+			if (tail != NULL)
+			{
+				tail->next = freeBlock;
+			}
+			// Update tail
+
+			tail = freeBlock;
+			printf("malloc %zu bytes\n", s);
+			pthread_mutex_unlock(&global_lock);
+
+			return (freeBlock + 1);
+		}
+
+		// Else if the sbrk doesn't work, then return NULL
+		else
+		{
+			pthread_mutex_unlock(&global_lock);
+			return NULL;
+		}
+	}
+}
+
+// A function that would find the best-fit for the calloc.
+void *mycalloc(size_t num, size_t s)
+{
+	// If the size of the array or the data allocated is 0, return null.
+	if (!num || !s)
+	{
+		return NULL;
+	}
+
+	// Else just malloc the total size of the entire array.
+	size_t totalSize = num * s;
+	block_t *freeBlock;
+	freeBlock = mymalloc(totalSize);
+	printf("calloc %zu bytes\n", totalSize);
+	memset(freeBlock, 0, totalSize);
+	return freeBlock;
+}
+
+// A function that would free the function
+void myfree(block_t *ptr)
+{
+	if (!ptr)
+	{
 		return;
 	}
-	header->s.is_free = 1;
-	pthread_mutex_unlock(&global_malloc_lock);
-}
 
-void *malloc(size_t size)
-{
-	size_t total_size;
-	void *block;
-	header_t *header;
-	if (!size)
-		return NULL;
-	pthread_mutex_lock(&global_malloc_lock);
-	header = get_free_block(size);
-	if (header) {
-		/* Woah, found a free block to accomodate requested memory. */
-		header->s.is_free = 0;
-		pthread_mutex_unlock(&global_malloc_lock);
-		return (void*)(header + 1);
-	}
-	/* We need to get memory to fit in the requested block and header from OS. */
-	total_size = sizeof(header_t) + size;
-	block = sbrk(total_size);
-	if (block == (void*) -1) {
-		pthread_mutex_unlock(&global_malloc_lock);
-		return NULL;
-	}
-	header = block;
-	header->s.size = size;
-	header->s.is_free = 0;
-	header->s.next = NULL;
-	if (!head)
-		head = header;
-	if (tail)
-		tail->s.next = header;
-	tail = header;
-	pthread_mutex_unlock(&global_malloc_lock);
-	return (void*)(header + 1);
-}
-
-void *calloc(size_t num, size_t nsize)
-{
-	size_t size;
-	void *block;
-	if (!num || !nsize)
-		return NULL;
-	size = num * nsize;
-	/* check mul overflow */
-	if (nsize != size / num)
-		return NULL;
-	block = malloc(size);
-	if (!block)
-		return NULL;
-	memset(block, 0, size);
-	return block;
-}
-
-void *realloc(void *block, size_t size)
-{
-	header_t *header;
-	void *ret;
-	if (!block || !size)
-		return malloc(size);
-	header = (header_t*)block - 1;
-	if (header->s.size >= size)
-		return block;
-	ret = malloc(size);
-	if (ret) {
-		/* Relocate contents to the new bigger block */
-		memcpy(ret, block, header->s.size);
-		/* Free the old memory block */
-		free(block);
-	}
-	return ret;
-}
-
-/* A debug function to print the entire link list */
-void print_mem_list()
-{
-	header_t *curr = head;
-	printf("head = %p, tail = %p \n", (void*)head, (void*)tail);
-	while(curr) {
-		printf("addr = %p, size = %zu, is_free=%u, next=%p\n",
-			(void*)curr, curr->s.size, curr->s.is_free, (void*)curr->s.next);
-		curr = curr->s.next;
-	}
+	block_t *delete;
+	delete = (block_t *)ptr - 1;
+	delete->free = 1;
+	printf("Freed: %zu\n", delete->size);
 }
